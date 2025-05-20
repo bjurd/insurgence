164e234e7dd7110dc61898693c23d493d5fe9f2b
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-20 19:08:09 -0400

    Translate Valve mathlib

---
+++ insurgence/valve/math.cpp
@@ -0,0 +1,122 @@
+#include "math.h"
+
+#include <cmath>
+#include <cfloat>
+
+void CrossProduct(const Vector& A, const Vector& B, Vector& Cross)
+{
+	Cross.x = A.y * B.z - A.z * B.y;
+	Cross.y = A.z * B.x - A.x * B.z;
+	Cross.z = A.x * B.y - A.y * B.z;
+}
+
+float __fastcall VectorNormalize(Vector& Target)
+{
+	float Radius = sqrtf(Target.x * Target.x + Target.y * Target.y + Target.z * Target.z);
+	float iRadius = 1.f / (Radius + FLT_EPSILON);
+
+	Target.x *= iRadius;
+	Target.y *= iRadius;
+	Target.z *= iRadius;
+
+	return Radius;
+}
+
+void VectorVectors(const Vector& Forward, Vector& Right, Vector& Up)
+{
+	Vector Temp;
+
+	if (Forward.x == 0.f && Forward.y == 0.f)
+	{
+		Right.x = 0.f;
+		Right.y = -1.f;
+		Right.z = 0.f;
+
+		Up.x = -Forward.z;
+		Up.y = 0.f;
+		Up.z = 0.f;
+	}
+	else
+	{
+		Temp.x = 0.f; Temp.y = 0.f; Temp.z = 1.f;
+
+		CrossProduct(Forward, Temp, Right);
+		VectorNormalize(Right);
+
+		CrossProduct(Right, Forward, Up);
+		VectorNormalize(Up);
+	}
+}
+
+void VectorAngles(const Vector& Forward, Angle& Angles)
+{
+	float Tmp, Yaw, Pitch;
+
+	if (Forward.x == 0.f && Forward.y == 0.f)
+	{
+		Yaw = 0.f;
+
+		if (Forward.z > 0.f)
+			Pitch = 270.f;
+		else
+			Pitch = 90.f;
+	}
+	else
+	{
+		Yaw = RAD2DEG(atan2f(Forward.y, Forward.x));
+
+		if (Yaw < 0.f)
+			Yaw += 360.f;
+
+		Tmp = sqrtf(Forward.x * Forward.x + Forward.y * Forward.y);
+		Pitch = RAD2DEG(atan2f(-Forward.z, Tmp));
+
+		if (Pitch < 0.f)
+			Pitch += 360.f;
+	}
+
+	Angles.p = Pitch;
+	Angles.y = Yaw;
+	Angles.r = 0.f;
+}
+
+void AngleVectors(const Angle& Angles, Vector& Forward)
+{
+	float RadPitch = DEG2RAD(Angles.p);
+	float RadYaw = DEG2RAD(Angles.y);
+
+	float Sp = sinf(RadPitch);
+	float Cp = cosf(RadPitch);
+	float Sy = sinf(RadYaw);
+	float Cy = cosf(RadYaw);
+
+	Forward.x = Cp * Cy;
+	Forward.y = Cp * Sy;
+	Forward.z = -Sp;
+}
+
+void AngleVectors(const Angle& Angles, Vector& Forward, Vector& Right, Vector& Up)
+{
+	float RadPitch = DEG2RAD(Angles.p);
+	float RadYaw = DEG2RAD(Angles.y);
+	float RadRoll = DEG2RAD(Angles.r);
+
+	float Sp = sinf(RadPitch);
+	float Cp = cosf(RadPitch);
+	float Sy = sinf(RadYaw);
+	float Cy = cosf(RadYaw);
+	float Sr = sinf(RadRoll);
+	float Cr = cosf(RadRoll);
+
+	Forward.x = Cp * Cy;
+	Forward.y = Cp * Sy;
+	Forward.z = -Sp;
+
+	Right.x = -Sr * Sp * Cy + Cr * Sy;
+	Right.y = -Sr * Sp * Sy - Cr * Cy;
+	Right.z = -Sr * Cp;
+
+	Up.x = Cr * Sp * Cy + Sr * Sy;
+	Up.y = Cr * Sp * Sy - Sr * Cy;
+	Up.z = Cr * Cp;
+}
---
+++ insurgence/valve/math.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "vector.h"
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#define DEG2RAD(x) ((x) * ((float)M_PI / 180.0f))
+#define RAD2DEG(x) ((x) * (180.0f / (float)M_PI))
+
+void CrossProduct(const Vector& A, const Vector& B, Vector& Cross);
+float __fastcall VectorNormalize(Vector& Target);
+
+void VectorVectors(const Vector& Forward, Vector& Right, Vector& Up);
+void VectorAngles(const Vector& Forward, Angle& Angles);
+
+void AngleVectors(const Angle& Angles, Vector& Forward);
+void AngleVectors(const Angle& Angles, Vector& Forward, Vector& Right, Vector& Up);
