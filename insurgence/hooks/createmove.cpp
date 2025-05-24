f23fc9b01e3e6c926cc9149a6bf4001e081a0cdd
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-24 03:03:18 -0400

    Slightly less humble aimbot
    Still wrong, think bone struct is incorrect

--- insurgence/hooks/createmove.cpp
+++ insurgence/hooks/createmove.cpp
@@ -7,13 +7,73 @@
 #include "../signatures.h"
 #include "../nwi/c_insplayer.h"
 #include "../valve/studio.h"
+#include "../helpers.h"
 
 typedef bool (*fnCreateMove)(void* _this, float SampleTime, CUserCmd* Command);
 fnCreateMove oCreateMove;
 
-void AimAtPlayer(CUserCmd* Command, C_INSPlayer* Target, studiohdr_t* Studio, const model_t* Model)
+C_INSPlayer* GetAimbotTarget()
 {
-	C_BaseEntity* LocalPlayer = (C_BaseEntity*)Globals->PointersManager->EntityList->GetClientEntity(Globals->PointersManager->Client->GetLocalPlayerIndex());
+	C_INSPlayer* Closest = nullptr;
+	float Distance = FLT_MAX;
+
+	C_INSPlayer* LocalPlayer = Helpers::GetLocalPlayer();
+	Vector LocalPlayerOrigin = LocalPlayer->GetAbsOrigin();
+
+	int Entities = Globals->PointersManager->EntityList->GetHighestEntityIndex();
+
+	for (int i = 1; i < Entities; ++i)
+	{
+		if (i == Globals->PointersManager->Client->GetLocalPlayerIndex())
+			continue;
+
+		C_BaseEntity* Ent = (C_BaseEntity*)Globals->PointersManager->EntityList->GetClientEntity(i);
+
+		if (!Ent)
+			continue;
+
+		if (strcmp(Ent->GetClassName(), "C_INSPlayer")) // TODO: IsPlayer
+			continue;
+
+		IClientRenderable* Renderable = Ent->GetClientRenderable();
+
+		if (!Renderable)
+			continue;
+
+		const model_t* Model = Renderable->GetModel();
+
+		if (!Model)
+			continue;
+
+		studiohdr_t* Studio = Globals->PointersManager->ModelInfo->GetStudiomodel(Model);
+
+		if (!Studio)
+			continue;
+
+		float CurrentDistance = Ent->GetAbsOrigin().DistToSqr(LocalPlayerOrigin);
+
+		if (CurrentDistance < Distance)
+		{
+			Distance = CurrentDistance;
+			Closest = (C_INSPlayer*)Ent;
+		}
+	}
+
+	return Closest;
+}
+
+Vector GetTargetAimPosition(C_INSPlayer* Target)
+{
+	Vector AimPos;
+
+	IClientRenderable* Renderable = Target->GetClientRenderable();
+	if (!Renderable) return AimPos;
+
+	const model_t* Model = Renderable->GetModel();
+	if (!Model) return AimPos;
+
+	studiohdr_t* Studio = Globals->PointersManager->ModelInfo->GetStudiomodel(Model);
+	if (!Studio) return AimPos;
 
 	matrix3x4_t Bones;
 	Target->SetupBones(&Bones, Studio->numbones, BONE_USED_BY_ANYTHING_AT_LOD(0), 0.f);
@@ -28,54 +88,30 @@ void AimAtPlayer(CUserCmd* Command, C_INSPlayer* Target, studiohdr_t* Studio, co
 		Vector Origin = Bone->pos;
 		Angle Rotation = Bone->rot.ToAngle();
 
-		Vector AimDir = Origin - LocalPlayer->GetAbsOrigin();
+		std::cout << std::hex << Bone << std::endl;
 
-		Angle AimAng;
-		VectorAngles(AimDir, AimAng);
-
-		Command->ViewAngles = AimAng;
-		return;
+		return Origin;
 	}
 }
 
 bool __fastcall hkCreateMove(void* _this, float SampleTime, CUserCmd* Command)
 {
-	C_BaseEntity* LocalPlayer = (C_BaseEntity*)Globals->PointersManager->EntityList->GetClientEntity(Globals->PointersManager->Client->GetLocalPlayerIndex());
-
-	if (!LocalPlayer)
-		return oCreateMove(_this, SampleTime, Command);
-
-	int Entities = Globals->PointersManager->EntityList->GetHighestEntityIndex();
-
-	for (int i = 1; i < Entities; ++i)
+	if (GetAsyncKeyState(VK_XBUTTON2) && Command->CommandNumber > 0 && Command->TickCount > 0)
 	{
-		C_BaseEntity* Ent = (C_BaseEntity*)Globals->PointersManager->EntityList->GetClientEntity(i);
+		C_INSPlayer* Target = GetAimbotTarget();
 
-		if (!Ent)
-			continue;
+		if (Target)
+		{
+			C_INSPlayer* LocalPlayer = Helpers::GetLocalPlayer();
 
-		if (strcmp(Ent->GetClassName(), "C_INSPlayer")) // TODO: IsPlayer
-			continue;
+			Vector AimPos = GetTargetAimPosition(Target);
+			Vector AimDir = AimPos - LocalPlayer->GetAbsOrigin();
 
-		IClientRenderable* Renderable = Ent->GetClientRenderable();
-		
-		if (!Renderable)
-			continue;
+			Angle AimAng;
+			VectorAngles(AimDir, AimAng);
 
-		const model_t* Model = Renderable->GetModel();
-
-		if (!Model)
-			continue;
-
-		studiohdr_t* Studio = Globals->PointersManager->ModelInfo->GetStudiomodel(Model);
-
-		if (!Studio)
-			continue;
-
-		AimAtPlayer(Command, (C_INSPlayer*)Ent, Studio, Model);
-		break;
-
-		//printf("got model for %d %s\n", i, Studio->name);
+			Command->ViewAngles = AimAng;
+		}
 	}
 
 	return oCreateMove(_this, SampleTime, Command);
