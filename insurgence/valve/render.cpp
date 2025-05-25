60d066129e1143a21dcae0cc3b89489240c206ad
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-25 13:01:57 -0400

    Improve speed of GetMainViewOrigin
    Smaller sig with relative grab of method

--- insurgence/valve/render.cpp
+++ insurgence/valve/render.cpp
@@ -2,7 +2,7 @@
 
 const Vector& GetMainViewOrigin()
 {
-	static uintptr_t GetMainViewOriginAddr = Memory::FindSignature("client.dll", Render_GetMainViewOrigin);
+	static uintptr_t GetMainViewOriginAddr = Memory::RelativeToReal(Memory::FindSignature("client.dll", Render_GetMainViewOrigin_Call), 1, 5);
 	using GetMainViewOriginFn = const Vector& (*)(int _);
 
 	static GetMainViewOriginFn FnGetMainViewOrigin = reinterpret_cast<GetMainViewOriginFn>(GetMainViewOriginAddr);
