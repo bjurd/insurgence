0d64f03759e25a4788996081939dc1df1912a605
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-25 10:13:22 -0400

    Remove extra space under Helpers::GetLocalPlayerEntity

--- insurgence/helpers.cpp
+++ insurgence/helpers.cpp
@@ -3,7 +3,6 @@
 #include "globals.h"
 
 C_BaseEntity* Helpers::GetLocalPlayerEntity()
-
 {
 	return (C_BaseEntity*)Globals->PointersManager->EntityList->GetClientEntity(Globals->PointersManager->Client->GetLocalPlayerIndex());
 }
