17d18cd31a48a78f4e5e7a136129c831f0ad0311
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-24 21:31:19 -0400

    Get proper ent index offset
    I think it's proper anyway

--- insurgence/valve/c_baseentity.h
+++ insurgence/valve/c_baseentity.h
@@ -11,6 +11,10 @@
 class C_BaseEntity : public IClientEntity
 {
 public:
+	// 0x48, 0x50, 0x8C
+	PROXYVAR(EntIndex, int, 0x008C)
+
+public:
 	CLPROXY(GetAbsOrigin, C_BaseEntity_GetAbsOrigin, Vector&, (void))
 	CLPROXY(GetAbsAngles, C_BaseEntity_GetAbsAngles, Angle&, (void))
 	VPROXY(GetPredDescMap, 17, datamap_t*, (void))
