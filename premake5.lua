990003d85f43e10fb22a053c0f05df95a0379744
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-28 22:26:35 -0400

    Create premake5 build script

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+workspace "Insurgence"
+	configurations { "Debug", "Release" }
+	platforms { "x64" }
+	startproject "Insurgence"
+
+project "Insurgence"
+	location "build"
+	kind "SharedLib"
+	language "C++"
+	cppdialect "C++20"
+	system "Windows"
+	systemversion "latest"
+	defines { "WIN32_LEAN_AND_MEAN" }
+
+	files { "insurgence/**.h", "insurgence/**.hpp", "insurgence/**.cpp", "insurgence/**.c" }
+	includedirs { "$(DXSDK_DIR)Include" }
+	libdirs { "$(DXSDK_DIR)Lib\\x64" }
+	links { "d3d9.lib", "d3dx9.lib", "dxerr.lib", "dxguid.lib" }
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+	filter {}
+
+	vpaths {
+		["Source Files/*"] = { "insurgence/**.cpp", "insurgence/**.c" },
+		["Header Files/*"] = { "insurgence/**.h", "insurgence/**.hpp" }
+	}
