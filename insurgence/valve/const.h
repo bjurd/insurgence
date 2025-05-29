f005c2947ae3a3073aa41ca3e5cc22b2c2ba0fff
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-29 01:55:03 -0400

    Add MoveType, MoveCollide, SolidType and SolidFlags

--- insurgence/valve/const.h
+++ insurgence/valve/const.h
@@ -1,5 +1,79 @@
 #pragma once
 
+// edict->movetype values
+enum MoveType_t
+{
+	MOVETYPE_NONE = 0,	// never moves
+	MOVETYPE_ISOMETRIC,			// For players -- in TF2 commander view, etc.
+	MOVETYPE_WALK,				// Player only - moving on the ground
+	MOVETYPE_STEP,				// gravity, special edge handling -- monsters use this
+	MOVETYPE_FLY,				// No gravity, but still collides with stuff
+	MOVETYPE_FLYGRAVITY,		// flies through the air + is affected by gravity
+	MOVETYPE_VPHYSICS,			// uses VPHYSICS for simulation
+	MOVETYPE_PUSH,				// no clip to world, push and crush
+	MOVETYPE_NOCLIP,			// No gravity, no collisions, still do velocity/avelocity
+	MOVETYPE_LADDER,			// Used by players only when going onto a ladder
+	MOVETYPE_OBSERVER,			// Observer movement, depends on player's observer mode
+	MOVETYPE_CUSTOM,			// Allows the entity to describe its own physics
+
+	// should always be defined as the last item in the list
+	MOVETYPE_LAST = MOVETYPE_CUSTOM,
+
+	MOVETYPE_MAX_BITS = 4
+};
+
+// edict->movecollide values
+enum MoveCollide_t
+{
+	MOVECOLLIDE_DEFAULT = 0,
+
+	// These ones only work for MOVETYPE_FLY + MOVETYPE_FLYGRAVITY
+	MOVECOLLIDE_FLY_BOUNCE,	// bounces, reflects, based on elasticity of surface and object - applies friction (adjust velocity)
+	MOVECOLLIDE_FLY_CUSTOM,	// Touch() will modify the velocity however it likes
+	MOVECOLLIDE_FLY_SLIDE,  // slides along surfaces (no bounce) - applies friciton (adjusts velocity)
+
+	MOVECOLLIDE_COUNT,		// Number of different movecollides
+
+	// When adding new movecollide types, make sure this is correct
+	MOVECOLLIDE_MAX_BITS = 3
+};
+
+// edict->solid values
+// NOTE: Some movetypes will cause collisions independent of SOLID_NOT/SOLID_TRIGGER when the entity moves
+// SOLID only effects OTHER entities colliding with this one when they move - UGH!
+
+// Solid type basically describes how the bounding volume of the object is represented
+// NOTE: SOLID_BBOX MUST BE 2, and SOLID_VPHYSICS MUST BE 6
+// NOTE: These numerical values are used in the FGD by the prop code (see prop_dynamic)
+enum SolidType_t
+{
+	SOLID_NONE = 0,	// no solid model
+	SOLID_BSP = 1,	// a BSP tree
+	SOLID_BBOX = 2,	// an AABB
+	SOLID_OBB = 3,	// an OBB (not implemented yet)
+	SOLID_OBB_YAW = 4,	// an OBB, constrained so that it can only yaw
+	SOLID_CUSTOM = 5,	// Always call into the entity for tests
+	SOLID_VPHYSICS = 6,	// solid vphysics object, get vcollide from the model and collide with that
+	SOLID_LAST,
+};
+
+enum SolidFlags_t
+{
+	FSOLID_CUSTOMRAYTEST = 0x0001,	// Ignore solid type + always call into the entity for ray tests
+	FSOLID_CUSTOMBOXTEST = 0x0002,	// Ignore solid type + always call into the entity for swept box tests
+	FSOLID_NOT_SOLID = 0x0004,	// Are we currently not solid?
+	FSOLID_TRIGGER = 0x0008,	// This is something may be collideable but fires touch functions
+	// even when it's not collideable (when the FSOLID_NOT_SOLID flag is set)
+	FSOLID_NOT_STANDABLE = 0x0010,	// You can't stand on this
+	FSOLID_VOLUME_CONTENTS = 0x0020,	// Contains volumetric contents (like water)
+	FSOLID_FORCE_WORLD_ALIGNED = 0x0040,	// Forces the collision rep to be world-aligned even if it's SOLID_BSP or SOLID_VPHYSICS
+	FSOLID_USE_TRIGGER_BOUNDS = 0x0080,	// Uses a special trigger bounds separate from the normal OBB
+	FSOLID_ROOT_PARENT_ALIGNED = 0x0100,	// Collisions are defined in root parent's local coordinate space
+	FSOLID_TRIGGER_TOUCH_DEBRIS = 0x0200,	// This trigger will touch debris objects
+
+	FSOLID_MAX_BITS = 10
+};
+
 enum Collision_Group_t
 {
 	COLLISION_GROUP_NONE = 0,
