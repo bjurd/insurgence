33a7937e4c550390270974fc29b7fc70495a979b
bjurd <177191469+bjurd@users.noreply.github.com>
2025-06-08 00:32:20 -0400

    Add getter to Hooks

--- insurgence/hooks.h
+++ insurgence/hooks.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <algorithm>
 #include <vector>
 
 class Hook
 {
 public:
+	virtual ~Hook() = default; // Dumb language
+
 	virtual void Create() = 0;
 	virtual void Destroy() = 0;
 };
@@ -17,6 +20,20 @@ public:
 public:
 	bool Create();
 	void Destroy();
+
+	template <typename T>
+	T* Get()
+	{
+		for (Hook* Hook : this->List)
+		{
+			T* Found = dynamic_cast<T*>(Hook);
+
+			if (Found)
+				return Found;
+		}
+
+		return nullptr;
+	}
 };
 
 inline Hooks* g_Hooks;
