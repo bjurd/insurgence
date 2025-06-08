a60b1a3e3a9ccdd38b75229c5d962bcd77f09743
bjurd <177191469+bjurd@users.noreply.github.com>
2025-06-07 21:02:57 -0400

    ENABLE_VIRTUAL_TERMINAL_PROCESSING on the external console
    Allows ANSI escape code colors

--- insurgence/console.cpp
+++ insurgence/console.cpp
@@ -7,6 +7,15 @@ void Console::Create()
 	if (AllocConsole())
 	{
 		freopen_s(&this->Stream, "CONOUT$", "w", stdout);
+
+		HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+		if (hStdOut != INVALID_HANDLE_VALUE)
+		{
+			DWORD ConsoleMode;
+			GetConsoleMode(hStdOut, &ConsoleMode);
+			SetConsoleMode(hStdOut, ConsoleMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+		}
 	}
 }
 
